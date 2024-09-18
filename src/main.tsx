b3dc5be5c8a8dd5edb22dbede3ab84be06ae4167
ana <imhannaah@gmail.com>
2024-09-18 13:29:46 -0300

    primeiro commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App /> 
+    </QueryClientProvider>
+    </StrictMode>,
+)
